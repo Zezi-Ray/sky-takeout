61b43217d0161abd4806a36fb43bfe2fd9679183
Ray <qiiqiriweoo@gmail.com>
2025-12-02 18:29:38 +0800

    add to shoppingcart

---
+++ takeout/sky-take-out/sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -0,0 +1,35 @@
+package com.sky.mapper;
+
+import com.sky.entity.ShoppingCart;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface ShoppingCartMapper {
+
+    /**
+     * 查询购物车列表
+     * @param shoppingCart 购物车信息
+     * @return 购物车列表
+     */
+    List<ShoppingCart> list(ShoppingCart shoppingCart);
+
+    /**
+     * 根据id更新购物车中菜品或套餐的数量
+     * @param shoppingCart 购物车信息
+     */
+    @Select("UPDATE shopping_cart SET number = #{number} WHERE id = #{id}")
+    void updateNumberById(ShoppingCart shoppingCart);
+
+    /**
+     * 插入购物车记录
+     * @param shoppingCart 购物车信息
+     */
+    @Insert("INSERT INTO shopping_cart " +
+            "(user_id, dish_id, setmeal_id, name, image, amount, number, create_time, dish_flavor) " +
+            "VALUES (#{userId}, #{dishId}, #{setmealId}, #{name}, #{image}, #{amount}, #{number}, #{createTime}, #{dishFlavor})")
+    void insert(ShoppingCart shoppingCart);
+}
