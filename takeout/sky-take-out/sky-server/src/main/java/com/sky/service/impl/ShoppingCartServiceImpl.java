9d496d2b712f0e17a75d0f58ffbc8a9ceaad7f2a
Ray <qiiqiriweoo@gmail.com>
2025-12-03 09:31:45 +0800

    view shopping cart

--- takeout/sky-take-out/sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
+++ takeout/sky-take-out/sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -76,4 +76,20 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
         }
 
     }
+
+    /**
+     * 查看购物车
+     * @return 购物车列表
+     */
+    public List<ShoppingCart> listShoppingCarts() {
+        // 获取当前用户id
+        Long userId = BaseContext.getCurrentId();
+        ShoppingCart shoppingCart = ShoppingCart.builder()
+                        .userId(userId)
+                        .build();
+        List<ShoppingCart> list = shoppingCartMapper.list(shoppingCart);
+        return list;
+
+    }
+
 }
